da81676da5c16adccf416867b9e4873869698350
sadmanryanriad <sadmanryanriad@gmail.com>
2023-10-23 13:43:36 +0600

    updated aboutme and banner texts and some small responsiveness issue for banner

--- src/Components/Home/sections/Banner.jsx
+++ src/Components/Home/sections/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <>
       <div
-        className="hero max-w-7xl w-full mx-auto h-[60vh] md:h-[80vh]"
+        className="hero max-w-7xl w-full mx-auto h-[65vh] md:h-[80vh]"
         style={{
           backgroundImage: `url(${bannerImage2})`,
         }}
@@ -13,14 +13,10 @@ const Banner = () => {
         <div className="hero-content text-center text-base-300">
           <div className="w-full">
             <h1 className="mb-5 text-2xl md:text-5xl font-bold w-full md:w-[90%] mx-auto">
-              Elevate your brand with captivating visuals that leave a lasting
-              impression
+            Elevating Digital Success: <span className="text-gray-700">Your Web Analytics Expert</span>
             </h1>
             <p className="mb-5 font-semibold w-full md:w-2/3 mx-auto md:text-xl">
-              I am your solution to businesses and companies, creating marketing
-              collateral that turns heads and drives results. From striking
-              logos to eye-catching brochures, we craft designs that resonate
-              with your audience and amplify your message.
+            Unlock the Power of Data with Our Web Analytics Specialist! 📈 Dive into the world of web analytics, tag management, and data-driven insights with our expert, Shahriar Talha. As a seasoned professional in the realm of digital intelligence, Shahriar combines creativity with strategy to provide you with actionable data solutions that drive results. Explore the art of data visualization, conversion optimization, and the intricacies of e-commerce analytics. Lets transform your online presence into a data-driven success story!
             </p>
             <button className="btn btn-btn btn-sm sm:btn-md bg-gray-700 text-white border-none">
               Get Started
--- src/Components/Home/sections/aboutMe/AboutMe.jsx
+++ src/Components/Home/sections/aboutMe/AboutMe.jsx
@@ -1,23 +1,37 @@
 import talhaPic from "../../../../assets/IMG_2598__1_-removebg-preview.png";
 
 const AboutMe = () => {
-    return (
-        <div id="about-me" className="bg-gray-100  dark:bg-gray-700  sm:p-4  pt-8 md:pt-12">
-            <div className="max-w-7xl mx-auto flex flex-col-reverse md:flex-row items-center text-center p-5">
-                <div className="md:w-3/5 sm:p-5 text-gray-700">
-                    <h2 className="text-2xl lg:text-3xl font-bold mb-5 mt-5 dark:text-slate-300">About me</h2>
-                    <h2 className="text-4xl lg:text-5xl font-bold mb-2 mt-5 dark:text-slate-300">Shahriar <span className="text-orange-400">Talha</span></h2>
-                    <h5 className=" font-semibold mb-5 dark:text-slate-300"> Expert in Web Analytics</h5>
-                    <p className="text-lg md:text-xl dark:text-slate-300">I blend creativity with strategy to create designs that not just look good, but also deliver results. With a passion for visual storytelling and a keen eye for design, I transform ideas into captivating visuals. My designs not only catch the eye but also convey messages effectively.</p>
-                </div>
-                <div className="md:w-2/5">
-                    <div className="overflow-hidden">
-                        <img className="w-full" src={talhaPic} alt="Shahriar Talha" />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div
+      id="about-me"
+      className="bg-gray-100  dark:bg-gray-700  sm:p-4  pt-8 md:pt-12"
+    >
+      <div className="max-w-7xl mx-auto flex flex-col-reverse md:flex-row items-center text-center p-5">
+        <div className="md:w-3/5 sm:p-5 text-gray-700">
+          <h2 className="text-2xl lg:text-3xl font-bold mb-5 mt-5 dark:text-slate-300">
+            About me
+          </h2>
+          <h2 className="text-4xl lg:text-5xl font-bold mb-2 mt-5 dark:text-slate-300">
+            Shahriar Arafat <span className="text-orange-400">Talha</span>
+          </h2>
+          <h5 className=" font-semibold mb-5 dark:text-slate-300">
+            {" "}
+            Expert in Web Analytics
+          </h5>
+          <p id="bio" className="text-lg md:text-xl dark:text-slate-300">
+            With 5+ years of web analytics expertise, Im Shahriar Talha 🚀.
+            Transforming data into insights and driving online success is my
+            passion. Lets decode your digital journey together! 📊
+          </p>
         </div>
-    );
+        <div className="md:w-2/5">
+          <div className="overflow-hidden">
+            <img className="w-full" src={talhaPic} alt="Shahriar Talha" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutMe;
